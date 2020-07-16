de2374ff83408f9a59ee6317fc8a746ef9a03677
MilesManners <doctor.miles.manners@gmail.com>
2020-07-16 10:09:40 -0500

    Modularize the server, add static routing

--- server/src/bin/main.rs
+++ server/src/bin/main.rs
@@ -1,40 +1,15 @@
-use std::fs;
-use std::io::prelude::*;
-use std::net::{TcpListener, TcpStream};
-
-use server::thread_pool::ThreadPool;
+use server::web_server::WebServer;
+use std::collections::HashMap;
 
 const THREAD_LIMIT: usize = 4;
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    let pool = ThreadPool::new(THREAD_LIMIT).unwrap();
+    let routes: HashMap<String, String> = [(String::from("/"), String::from("hello.html"))]
+        .iter()
+        .cloned()
+        .collect();
 
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
+    let server = WebServer::new(THREAD_LIMIT, routes);
 
-        pool.execute(|| {
-            handle_connection(stream);
-        })
-    }
-}
-
-fn handle_connection(mut stream: TcpStream) {
-    let mut buffer = [0; 512];
-    stream.read(&mut buffer).unwrap();
-
-    let get = b"GET / HTTP/1.1\r\n";
-
-    let (status_line, filename) = if buffer.starts_with(get) {
-        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
-    };
-
-    let contents = fs::read_to_string(filename).unwrap();
-
-    let response = format!("{}{}", status_line, contents);
-
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+    server.start("127.0.0.1:7878").unwrap();
 }
--- server/src/lib.rs
+++ server/src/lib.rs
@@ -1,3 +1,136 @@
+pub mod web_server {
+    use http::{Method, Request, Response, Version};
+    use std::{
+        collections::HashMap,
+        error::Error,
+        fs,
+        io::prelude::*,
+        net::{TcpListener, TcpStream},
+        sync::{Arc, RwLock},
+    };
+
+    use crate::thread_pool::ThreadPool;
+
+    type Routes = HashMap<String, String>;
+
+    pub struct WebServer {
+        thread_limit: usize,
+        routes: Arc<RwLock<Routes>>,
+    }
+
+    impl WebServer {
+        pub fn new(thread_limit: usize, routes: Routes) -> WebServer {
+            let routes = Arc::new(RwLock::new(routes));
+
+            WebServer {
+                thread_limit,
+                routes,
+            }
+        }
+
+        pub fn start(&self, ip: &str) -> Result<(), Box<dyn Error>> {
+            let listener = TcpListener::bind(ip)?;
+            let pool = ThreadPool::new(self.thread_limit)?;
+
+            for stream in listener.incoming() {
+                let stream = stream?;
+
+                let routes = Arc::clone(&self.routes);
+
+                pool.execute(|| {
+                    handle_connection(routes, stream).unwrap();
+                })
+            }
+
+            Ok(())
+        }
+    }
+
+    fn handle_connection(
+        routes: Arc<RwLock<Routes>>,
+        mut stream: TcpStream,
+    ) -> Result<(), Box<dyn Error>> {
+        let mut buffer = [0; 512];
+        stream.read(&mut buffer)?;
+
+        println!("{}\n", String::from_utf8_lossy(&buffer));
+
+        let lines: Vec<String> = buffer.lines().map(|line| line.unwrap()).collect();
+
+        let (method, uri, version) = match lines
+            .get(0)
+            .unwrap()
+            .split_whitespace()
+            .collect::<Vec<&str>>()
+            .get(0..2)
+            .unwrap()
+        {
+            &[a, b, c] => (a, b, c),
+            _ => unreachable!(),
+        };
+
+        let method = Method::from_bytes(method.as_bytes()).unwrap();
+
+        let version = match version {
+            "HTTP/0.9" => Version::HTTP_09,
+            "HTTP/1.0" => Version::HTTP_10,
+            "HTTP/1.1" => Version::HTTP_11,
+            "HTTP/2.0" => Version::HTTP_2,
+            "HTTP/3.0" => Version::HTTP_3,
+            _ => unreachable!(),
+        };
+
+        let request = Request::builder()
+            .method(method)
+            .uri(uri)
+            .version(version)
+            .body(vec![])
+            .unwrap();
+
+        let response = response(routes, request).unwrap();
+
+        let response = format!(
+            "{:?} {}\r\n\r\n{}",
+            response.version(),
+            response.status(),
+            response.body()
+        );
+
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+
+        Ok(())
+    }
+
+    fn response(
+        routes: Arc<RwLock<Routes>>,
+        request: Request<Vec<u8>>,
+    ) -> http::Result<Response<String>> {
+        let method = request.method();
+
+        match *method {
+            Method::GET | Method::POST => match routes.read().unwrap().get(request.uri().path()) {
+                Some(file) => {
+                    let body = fs::read_to_string(file).unwrap();
+
+                    Response::builder().status(200).body(body)
+                }
+                None => {
+                    let body = fs::read_to_string("404.html").unwrap();
+
+                    Response::builder().status(404).body(body)
+                }
+            },
+            Method::HEAD | Method::OPTIONS => Response::builder()
+                .status(501)
+                .body(format!("Server does not support {} requests", method)),
+            _ => Response::builder()
+                .status(405)
+                .body(format!("Server does not allow {} requests", method)),
+        }
+    }
+}
+
 pub mod thread_pool {
     use std::{
         error::Error,
@@ -64,7 +197,7 @@ pub mod thread_pool {
             println!("Shutting down all workers.");
 
             for worker in &mut self.workers {
-                println!("Shutting down worker {}", worker.id);
+                // println!("Shutting down worker {}", worker.id);
 
                 if let Some(thread) = worker.thread.take() {
                     thread.join().unwrap();
@@ -85,12 +218,12 @@ pub mod thread_pool {
 
                 match message {
                     Message::NewJob(job) => {
-                        println!("Worker {} got a job; executing.", id);
+                        // println!("Worker {} got a job; executing.", id);
 
                         job();
                     }
                     Message::Terminate => {
-                        println!("Worker {} was told to terminate.", id);
+                        // println!("Worker {} was told to terminate.", id);
 
                         break;
                     }
